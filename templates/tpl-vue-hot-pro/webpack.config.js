999fd916738be7572ba36d562aff302639d12b73
spck-io <spckorg@gmail.com>
2021-01-26 21:55:51 -0500

    Updates

---
+++ templates/tpl-vue-hot-pro/webpack.config.js
@@ -0,0 +1,60 @@
+const webpack = require('webpack');
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+            name: '[name].[ext]?[hash]',
+            esModule: false
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.vue'
+    ],
+    modules:[
+      'node_modules',
+      path.join(process.env.NPM_CONFIG_PREFIX || __dirname, 'lib/node_modules')
+    ]
+  },
+  resolveLoader: {
+    modules:[
+      'node_modules',
+      path.join(process.env.NPM_CONFIG_PREFIX || __dirname, 'lib/node_modules')
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ],
+  devServer: {
+    port: 9951,
+    contentBase: './dist',
+    writeToDisk: true
+  }
+};
+
+module.exports = config;
